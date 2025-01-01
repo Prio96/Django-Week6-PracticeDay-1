425bb2d0e42bb551a9a10ccca4856d27f389650c
Prio96 <prio96@icloud.com>
2025-01-01 12:21:39 +0600

    first commit

---
+++ transactions/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import DepositMoneyView, WithdrawMoneyView, TransactionReportView, LoanRequestView, LoanListView, RepayLoanView
+
+urlpatterns = [
+    path("deposit/",DepositMoneyView.as_view(), name="Deposit"),
+    path("report/",TransactionReportView.as_view(), name="Transaction Report"),
+    path("withdraw/",WithdrawMoneyView.as_view(), name="Withdraw"),
+    path("loan_request/",LoanRequestView.as_view(), name="Loan Request"),
+    path("loans/",LoanListView.as_view(), name="Loan List"),
+    path("loan/<int:loan_id>/",RepayLoanView.as_view(), name="Loan Repayment"),
+]
